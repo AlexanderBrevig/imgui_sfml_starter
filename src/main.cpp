ec64557bac7a361870149eaadae75b2b540956ff
Alexander Brevig <alexanderbrevig@gmail.com>
2019-03-18 19:47:20 +0100

    remove stupid opinionated MainMenuBar thingy

--- src/main.cpp
+++ src/main.cpp
@@ -9,32 +9,15 @@
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/OpenGL.hpp>
 
-#include "MainMenuBar.hpp"
-
-int main()
-{
+int main() {
     auto mode = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(mode, "imgui_sfml_starter");
     window.setView(sf::View(sf::FloatRect(0.f, 0.f, mode.width, mode.height)));
     window.setFramerateLimit(60);
     window.setVerticalSyncEnabled(true);
-    window.resetGLStates(); // call it if you only process ImGui. Otherwise not needed.
+    window.resetGLStates(); 
 
     ImGui::SFML::Init(window);
-
-    MainMenuBar menu;
-    //set boolean bindings
-    menu.setBinding("canUndo", false);
-    menu.setBinding("canRedo", false);
-    //populate list bindings
-    menu.setListBinding("recentFiles", {"file1.txt", "file2.txt"});
-
-    menu.observeState("recentFiles", [](std::string name, bool state) {
-        std::cout << "Open recent file: " << name << std::endl;
-    });
-    menu.observeState("New", [](std::string name, bool state) {
-        std::cout << "New file!" << std::endl;
-    });
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -51,7 +34,6 @@ int main()
         ImGui::SFML::Update(window, deltaTime);
         auto size = window.getSize();
 
-        menu.draw();
         auto windowPos = ImVec2(.0f, 20.0f);
         ImGui::SetNextWindowPos(windowPos);
         auto contentRegion = ImGui::GetWindowContentRegionMax();
@@ -61,30 +43,14 @@ int main()
         ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth());
 
         ImGui::Begin("Hello", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
-
+        ImGui::DrawLine(sf::Vector2<float>(0, 0), sf::Vector2<float>(20, 20), sf::Color::Blue);
+        
         ImGui::SetWindowFontScale(3);
         ImGui::Text("Welcome to this Dear ImGui & SFML boilerplate.");
         ImGui::SetWindowFontScale(1);
         if (ImGui::CollapsingHeader("Getting Started")) {
-            
-            menu.setBinding("canRedo", true); //simulate a change - now the File->Redo is enabled
             ImGui::Text("PROGRAMMER GUIDE:");
             ImGui::BulletText("Please see src/main.cpp");
-            ImGui::BulletText("Edit menu.json to suit your needs.");
-            ImGui::BulletText("Then add menu.observeState to get notified when item is selected");
-            ImGui::Text("FEATURE GUIDE:");
-            ImGui::BulletText("Please click File->README and witness the new README section below");
-        }
-
-        if (menu.has("README")) {
-            ImGui::Separator();
-            if (ImGui::CollapsingHeader("menu.has(\"README\")")) {
-                ImGui::Text("You can check for any state using menu.has(\"some string\")");
-                
-                if (ImGui::Button("menu.consume(\"README\")")){
-                    menu.consume("README");
-                }
-            }
         }
 
         ImGui::End();
@@ -95,6 +61,6 @@ int main()
     }
 
     ImGui::SFML::Shutdown();
-    
+
     return 0;
 }
